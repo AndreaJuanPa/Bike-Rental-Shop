21fc1b167e9fe79ff1d1a4ddc60c9fd3a159739f
AndreaJuanPa <130765602+AndreaJuanPa@users.noreply.github.com>
2023-05-10 10:23:28 -0400

    Update 2 Main

--- Main.py
+++ Main.py
@@ -1,24 +1,61 @@
-# Creating Parent Class
 class Bike:
-# Creating Methods:
-    def __init__(self,stock):
-        self.stock = stock
-        self.total_cost = 0
+    def __init__(self,inventory):
+        self.inventory = inventory
+        self.total_income = 0
 
-    def show_stock(self):
-        print("Cecilia's Bike Shop Inventory:")
+    def show_inventory(self):
+        print("Cecilia's Bike Rental Shop Inventory:")
 
-    def rent_hourly(self,number):
-       self.total_cost += number * 5
+    def hourly_type(self,number):
+        if number <= 0:
+            print("Invalid input")
+            return
 
-    def rent_daily(self, number):
-       self.total_cost += number * 20
+        if number > self.inventory["hourly_bikes"]:
+            print ("Sorry, we can't provide that quantity of bikes")
+            return
 
-    def rent_weekly(self,number):
-       self.total_cost += number * 60
+        print("Rented hourly type")
+        self.total_income += number * 5
+        self.inventory["hourly bikes"] -= number
+
+    def daily_type(self, number):
+        if number <= 0:
+            print("Invalid input")
+            return
+
+        if number > self.inventory["daily_bikes"]:
+            print("Sorry, we can't provide that quantity of bikes")
+            return
+
+         print("Rented daily type")
+         self.inventory["daily_bikes"] -= number
+         self.total_income += number * 20
+
+    def weekly_type(self,number):
+        if number <= 0:
+            print("Invalid input")
+            return
+
+        if number > self.inventory["weekly_bikes"]:
+            print("Sorry, we can't provide that quantity of bikes")
+            return
+
+        print("Rented weekly type")
+        self.inventory["weekly_bikes"] -= number
+        self.total_income += number * 60
 
     def family_rental(self,number):
-
+        if number <= 0:
+            print("Invalid input")
+            return
+        
+        if number >= 3 and number <= 5:
+        self.total_income +=
 
 #Creating Objects:
-bikes = Bike(200)
+hourly_bikes = Bike (200)
+daily_bikes = Bike (100)
+weekly_bikes = Bike (70)
+
+
